dc8d28a256a17526fdaec8bc3e115ac184b9195e
tanghc <thc8775@163.com>
2020-10-20 19:51:09 +0800

    支持docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 JAVA_OPTS="-Xms128m -Xmx128m"
 
 # mysql, nacos配置
-args="--mysql.host=10.1.30.110:3306 --register.url=10.1.30.110:8848"
+args="--mysql.host=10.1.30.110:3306 --mysql.username=root --mysql.password=root --register.url=10.1.30.110:8848"
 
 java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /sop/sop-gateway/sop-gateway.jar $args --logging.file.path=/sop/sop-gateway/log &
 java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /sop/sop-admin/sop-admin.jar $args --logging.file.path=/sop/sop-admin/log &
