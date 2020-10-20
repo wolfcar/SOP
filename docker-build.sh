58d42dc382d4ebd42cf85d1ade83f5437210fc95
tanghc <thc8775@163.com>
2020-10-20 10:58:02 +0800

    支持docker

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# 打包
 mvn clean package
-docker build -t sop .
+# 创建镜像
+docker build -t gitee.com/sop .
+
+# 获取镜像id
+image_id=`docker images gitee.com/sop --format "{{.ID}}" | awk '{print $1}'`
+
+# 运行镜像
+docker run --name sop -p 8081:8081 -p 8082:8082 -p 8083:8083 -p 2222:2222 -d $image_id
