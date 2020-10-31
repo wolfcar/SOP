ca6021a67c519b9a17fd24e713b821b5bf084ea1
tanghc <thc8775@163.com>
2020-11-01 07:57:49 +0800

    重构路由监控

--- sop-mysql5.6以下版本.sql
+++ sop-mysql5.6以下版本.sql
@@ -223,16 +223,17 @@ CREATE TABLE `monitor_info` (
   `total_request_count` bigint(20) NOT NULL DEFAULT '0' COMMENT '总调用次数',
   `success_count` bigint(20) NOT NULL DEFAULT '0' COMMENT '成功次数',
   `error_count` bigint(20) NOT NULL DEFAULT '0' COMMENT '失败次数（业务主动抛出的异常算作成功，如参数校验，未知的错误算失败）',
-  `gmt_create` DATETIME DEFAULT NULL,
-  `gmt_modified` DATETIME DEFAULT NULL,
+  `gmt_create` datetime DEFAULT DEFAULT NULL,
+  `gmt_modified` datetime DEFAULT DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_routeid` (`route_id`) USING BTREE,
+  UNIQUE KEY `uk_routeid` (`route_id`,`instance_id`) USING BTREE,
   KEY `idex_name` (`name`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='接口监控信息';
 
 CREATE TABLE `monitor_info_error` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `error_id` varchar(64) NOT NULL DEFAULT '' COMMENT '错误id,md5Hex(instanceId + routeId + errorMsg)',
+  `instance_id` varchar(128) NOT NULL DEFAULT '' COMMENT '实例id',
   `route_id` varchar(128) NOT NULL DEFAULT '',
   `error_msg` text NOT NULL,
   `error_status` int(11) NOT NULL DEFAULT '0' COMMENT 'http status，非200错误',
@@ -241,7 +242,7 @@ CREATE TABLE `monitor_info_error` (
   `gmt_create` DATETIME DEFAULT NULL,
   `gmt_modified` DATETIME DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_erririd` (`error_id`) USING BTREE,
+  UNIQUE KEY `uk_errorid` (`error_id`) USING BTREE,
   KEY `idx_routeid` (`route_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
